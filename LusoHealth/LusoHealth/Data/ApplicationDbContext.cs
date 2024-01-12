a3cd303b19697a82a10c1d7a7800c10e6a5829d9
jaime-vieira <101071059+jaime-vieira@users.noreply.github.com>
2024-01-12 22:41:37 +0000

    Base de Dados criada

--- LusoHealth/LusoHealth/Data/ApplicationDbContext.cs
+++ LusoHealth/LusoHealth/Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using LusoHealth.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LusoHealth.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
+        public DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {

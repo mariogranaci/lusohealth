0a61a1a40f80b95b71aed09dbc0344ab2be32e59
Francisco Vaz <202100217@estudantes.ips.pt>
2024-01-19 16:54:36 +0000

    navbar concluida

--- LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/navbar/navbar.component.ts
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/navbar/navbar.component.ts
@@ -3,20 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
   isDropdownOpen = false;
   activeDropdown: string | null = null;
 
   toggleDropdown(dropdown: string) {
-    if (this.activeDropdown === dropdown) {
-      this.isDropdownOpen = !this.isDropdownOpen;
-    } else {
-      this.isDropdownOpen = true;
-    }
-    this.activeDropdown = dropdown;
+    this.isDropdownOpen = !this.isDropdownOpen;
+    this.activeDropdown = this.isDropdownOpen ? dropdown : null;
   }
 }
-
-

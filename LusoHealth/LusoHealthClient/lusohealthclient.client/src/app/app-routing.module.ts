ee50232817294d1628a2c7f9cce5b21b7fb98b63
Francisco Vaz <202100217@estudantes.ips.pt>
2024-01-22 22:56:45 +0000

    updates

--- LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/app-routing.module.ts
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 //import { RegistoComponent } from './registo/registo.component';
 //import { LoginComponent } from './login/login.component';
+import { RecuperarPassComponent } from './recuperar-pass/recuperar-pass.component';
+import { AlterarPassComponent } from './alterar-pass/alterar-pass.component';
 
 const routes: Routes = [
-  { path: 'account', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) }
+  { path: 'account', loadChildren: () => import('./authentication/authentication.module').then(m => m.AuthenticationModule) },
   //{ path: 'register', component: RegistoComponent },
   //{ path: 'login', component: LoginComponent },
+  { path: 'recuperar-pass', component: RecuperarPassComponent },
+  { path: 'alterar-pass', component: RecuperarPassComponent },
+
 ];
 
 @NgModule({

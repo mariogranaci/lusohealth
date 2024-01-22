a19acf6b9b03c956b299aa048a6e585152fe77bd
Mário Granaci <202100275@estudantes.ips.pt>
2024-01-22 01:48:14 +0000

    login backend feito
    falta interligar o frontend e o backend

--- LusoHealth/LusoHealthClient/LusoHealthClient.Server/Program.cs
+++ LusoHealth/LusoHealthClient/LusoHealthClient.Server/Program.cs
@@ -1,5 +1,11 @@
 using LusoHealthClient.Server.Data;
+using LusoHealthClient.Server.Models;
+using LusoHealthClient.Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +21,46 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString)
 );
 
+// be able to inject the JWTService class inside the controllers
+builder.Services.AddScoped<JWTService>();
+
+builder.Services.AddIdentityCore<User>(options =>
+{
+    // Password settings 
+    options.Password.RequireDigit = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+
+    options.SignIn.RequireConfirmedAccount = true;
+})
+    .AddRoles<IdentityRole>()
+    .AddRoleManager<RoleManager<IdentityRole>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager<SignInManager<User>>()
+    .AddUserManager<UserManager<User>>()
+    .AddDefaultTokenProviders();
+
+// be able to authenticate the user with JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // validate the token based on the key we have in appsettings.json JWT:Key
+            ValidateIssuerSigningKey = true,
+            // the issuer singning key based on JWT:Key
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            // the issuer which in here is the api project url we are using
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            // validadate the issuer (who ever is issuing the JWT)
+            ValidateIssuer = true,
+            // dont validate the audience (angular side)
+            ValidateAudience = false
+        };
+    });
+
 var app = builder.Build();
 
 app.UseDefaultFiles();
@@ -28,6 +74,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+// adding UseAuthentication into our pipeline and should be before UseAuthorization
+// Authentication verifies the identity of the user or service, and authorization determines their access rights.
+app.UseAuthentication();
 
 app.UseAuthorization();
 

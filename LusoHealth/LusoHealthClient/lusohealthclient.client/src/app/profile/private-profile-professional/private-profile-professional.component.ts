2d9d1d9896fb5bbbf70d684eacab5644af87fae8
jaime-vieira <101071059+jaime-vieira@users.noreply.github.com>
2024-02-24 05:00:45 +0000

    updates

--- LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/profile/private-profile-professional/private-profile-professional.component.ts
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/profile/private-profile-professional/private-profile-professional.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../../shared/models/authentication/user';
+import { AuthenticationService } from '../../authentication/authentication.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-private-profile-professional',
@@ -6,8 +11,46 @@ import { Component } from '@angular/core';
   styleUrl: './private-profile-professional.component.css'
 })
 export class PrivateProfileProfessionalComponent {
+  addSpecialityForm: FormGroup = new FormGroup({});
+  submitted = false;
+  errorMessages: string[] = [];
+  responseText: string | undefined;
+
+  constructor(private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router) {
+    this.authenticationService.user$.pipe(take(1)).subscribe({
+      next: (user: User | null) => {
+        if (user) {
+          this.router.navigateByUrl('/');
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+
+    this.addSpecialityForm = this.formBuilder.group({
+
+      selectSpeciality: ['', [Validators.required]],
+      price: ['', [Validators.required, Validators.min(1), Validators.max(1000)]],
+      presencial: ['', [Validators.required]],
+      online: ['', [Validators.required]],
+      domicilio: ['', [Validators.required]]
+    })
+  }
+
+  addSpeciality() {
+    this.submitted = true;
+    this.errorMessages = [];
+    this.responseText = '';
 
 
+  }
 
   openPopup(opcao: string) {
     const overlay = document.getElementById('overlay');

2db92ad6ce4142c6e143d091eb3e9892c6cd5128
jaime-vieira <101071059+jaime-vieira@users.noreply.github.com>
2024-02-24 00:10:41 +0000

    popups a funcionar

--- LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/profile/private-profile-professional/private-profile-professional.component.ts
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/profile/private-profile-professional/private-profile-professional.component.ts
@@ -7,4 +7,52 @@ import { Component } from '@angular/core';
 })
 export class PrivateProfileProfessionalComponent {
 
+
+
+  openPopup(opcao: string) {
+    const overlay = document.getElementById('overlay');
+    const add = document.getElementById('add-speciality-container');
+    const edit = document.getElementById('edit-speciality-container');
+
+    if (edit) {
+      edit.style.display = "none";
+    }
+    if (add) {
+      add.style.display = "none";
+    }
+
+    if (overlay) {
+      overlay.style.display = 'flex';
+      if (opcao == "add") {
+        if (add) {
+          add.style.display = "block";
+        }
+      }
+      else if (opcao == "edit") {
+        if (edit) {
+          edit.style.display = "block";
+        }
+      }
+    }
+  }
+
+  closePopup() {
+    const overlay = document.getElementById('overlay');
+    const add = document.getElementById('add-speciality-container');
+    const edit = document.getElementById('edit-speciality-container');
+
+    if (overlay) {
+      overlay.style.display = 'none';
+      if (edit) {
+        edit.style.display = "none";
+      }
+      if (add) {
+        add.style.display = "none";
+      }
+    }
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
 }

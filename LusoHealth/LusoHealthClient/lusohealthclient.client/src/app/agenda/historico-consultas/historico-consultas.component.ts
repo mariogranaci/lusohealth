dabb767622806eba6f5da44a292ac5c22ad1787d
Tiago-Felix <101177022+Tiago-Felix@users.noreply.github.com>
2024-03-09 22:43:15 +0000

    criacao dos componentes agenda do paciente e historico de consultas

---
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/agenda/historico-consultas/historico-consultas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historico-consultas',
+  templateUrl: './historico-consultas.component.html',
+  styleUrl: './historico-consultas.component.css'
+})
+export class HistoricoConsultasComponent {
+
+}

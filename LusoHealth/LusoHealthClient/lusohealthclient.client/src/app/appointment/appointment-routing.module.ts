a5afedc7f4c1de5b536d7454e432a051155c3d0e
Tiago-Felix <101177022+Tiago-Felix@users.noreply.github.com>
2024-04-02 21:49:08 +0100

    kjh

--- LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/appointment/appointment-routing.module.ts
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/appointment/appointment-routing.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ConsultaProfissionalComponent } from './consulta-profissional/consulta-profissional.component';
 import { ConsultaPacienteComponent } from './consulta-paciente/consulta-paciente.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
   { path: 'patient-appointment', component: ConsultaPacienteComponent },
-  { path: 'professional-appointment', component: ConsultaProfissionalComponent }
+  { path: 'professional-appointment', component: ConsultaProfissionalComponent },
+  { path: 'chat', component: ChatComponent }
 ]
 
 @NgModule({

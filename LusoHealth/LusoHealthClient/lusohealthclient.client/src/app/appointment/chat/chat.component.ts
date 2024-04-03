7b676080cd4cdba761903df52732a69f56ec6d12
Tiago-Felix <101177022+Tiago-Felix@users.noreply.github.com>
2024-04-03 20:01:01 +0100

    Finalizacao do front end do chat, incluindo adicao de novos metodos no backend da appointment

--- LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/appointment/chat/chat.component.ts
+++ LusoHealth/LusoHealthClient/lusohealthclient.client/src/app/appointment/chat/chat.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { ProfileService } from '../../profile/profile.service';
+import { ServicesService } from '../../services/services.service';
+import { Professional } from '../../shared/models/profile/professional';
+import { UserProfile } from '../../shared/models/profile/userProfile';
+import { Appointment } from '../../shared/models/services/appointment';
+import { Service } from '../../shared/models/services/service';
+import { AppointmentService } from '../appointment.service';
+import { AgendaService } from '../../agenda/agenda.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
@@ -7,4 +18,230 @@ import { Component } from '@angular/core';
 })
 export class ChatComponent {
 
+  private unsubscribe$ = new Subject<void>();
+  errorMessages: string[] = [];
+  responseText: string = "";
+
+  service: Service | undefined;
+
+  appointmentId: number = this.route.snapshot.queryParams['appointment'];
+  appointment: Appointment | undefined;
+  professional: Professional | undefined;
+  patient: UserProfile | undefined;
+
+  chatEnabled = false;
+
+  isSender = false;
+
+  isSenderPatient = true;
+
+  constructor(public servicesService: ServicesService, public appointmentService: AppointmentService,
+    public agendaService: AgendaService, private route: ActivatedRoute, public profileService: ProfileService,
+    private location: Location)
+  {}
+
+  ngOnInit() {
+    this.getAppointmentInfo().then(() => {
+      this.getServiceInfo();
+      this.getProfessional();
+      this.getPatient();
+      this.getUser();
+      this.chatEnabled = (this.appointment?.state != 'Done' && this.appointment?.state != 'Canceled')
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  getAppointmentInfo(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.appointmentService.getAppointmentInfo(this.appointmentId).pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe({
+        next: (appointment: any) => {
+          this.appointment = appointment;
+          resolve();
+        },
+        error: (error) => {
+          console.log(error);
+          if (error.error.errors) {
+            this.errorMessages = error.error.errors;
+          } else {
+            this.errorMessages.push(error.error);
+          }
+          reject();
+        }
+      });
+    });
+  }
+
+  getServiceInfo() {
+    if (this.appointment && this.appointment.idService != null) {
+      this.servicesService.getServiceInfo(this.appointment.idService).pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe({
+        next: (service: any) => {
+          this.service = service;
+        },
+        error: (error) => {
+          console.log(error);
+          if (error.error.errors) {
+            this.errorMessages = error.error.errors;
+          } else {
+            this.errorMessages.push(error.error);
+          }
+        }
+      });
+    }
+  }
+
+  getProfessional() {
+    if (this.appointment && this.appointment.idProfessional != null) {
+      this.profileService.getProfessionalInfoById(this.appointment?.idProfessional).pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe({
+        next: (professional: any) => {
+          this.professional = professional;
+        },
+        error: (error) => {
+          console.log(error);
+          if (error.error.errors) {
+            this.errorMessages = error.error.errors;
+          } else {
+            this.errorMessages.push(error.error);
+          }
+        }
+      });
+    }
+  }
+
+  getPatient() {
+    if (this.appointment?.idPatient) {
+      this.profileService.getUserById(this.appointment?.idPatient).pipe(
+        takeUntil(this.unsubscribe$)
+      ).subscribe({
+        next: (patient: any) => {
+          this.patient = patient;
+        },
+        error: (error) => {
+          console.log(error);
+          if (error.error.errors) {
+            this.errorMessages = error.error.errors;
+          } else {
+            this.errorMessages.push(error.error);
+          }
+        }
+      });
+    }
+  }
+
+  getUser() {
+    // PARA O JAIME IR VER SE O USER E PATIENT OU NAO E DAR SET A VARIAVEL isSenderPatient
+    // PARA IR BUSCAR O NOME NO HTML
+  }
+
+  changeAppointmentDone() {
+    this.appointmentService.finishAppointment(this.appointment).pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe({
+      next: (appointment: any) => {
+        console.log("Appointment finished successfully:", appointment);
+        this.appointment = appointment;
+      },
+      error: (error) => {
+        console.log("Error finishing appointment:", error);
+        if (error.error.errors) {
+          this.errorMessages = error.error.errors;
+        } else {
+          this.errorMessages.push(error.error);
+        }
+      }
+    });
+  }
+
+  changeAppointmentBegin() {
+    this.appointmentService.beginAppointment(this.appointment).pipe(
+      takeUntil(this.unsubscribe$)
+    ).subscribe({
+      next: (appointment: any) => {
+        console.log("Appointment started successfully:", appointment);
+        this.appointment = appointment;
+      },
+      error: (error) => {
+        console.log("Error starting appointment:", error);
+        if (error.error.errors) {
+          this.errorMessages = error.error.errors;
+        } else {
+          this.errorMessages.push(error.error);
+        }
+      }
+    });
+  }
+
+  getProfessionalNameById(): string {
+    if (this.service?.professional) {
+      return this.service?.professional.professionalInfo.firstName + " " + this.service?.professional.professionalInfo.lastName;
+    }
+    return "";
+  }
+
+  getProfessionalById(): Professional | undefined {
+    return this.service?.professional;
+  }
+
+  openPopup(opcao: string) {
+    const overlay = document.getElementById('overlay');
+    const remove = document.getElementById('remove-appointment-container');
+
+    if (remove) {
+      remove.style.display = "none";
+    }
+
+    if (overlay) {
+      overlay.style.display = 'flex';
+      if (opcao == "remove") {
+        if (remove) {
+          remove.style.display = "block";
+        }
+      }
+    }
+  }
+
+  closePopup() {
+    const overlay = document.getElementById('overlay');
+    const add = document.getElementById('add-appointment-container');
+    const edit = document.getElementById('edit-appointment-container');
+
+    if (overlay) {
+      overlay.style.display = 'none';
+      if (edit) {
+        edit.style.display = "none";
+      }
+      if (add) {
+        add.style.display = "none";
+      }
+    }
+  }
+
+  endChat()
+  {
+    this.chatEnabled = false;
+    this.changeAppointmentDone();
+    this.closePopup();
+  }
+
+  startChat() {
+    this.chatEnabled = true;
+    this.changeAppointmentBegin();
+  }
+
+  stopPropagation(event: Event) {
+    event.stopPropagation();
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }

73c5a7e3d3fa07d91d6e0ee3af9d22cae17ddaec
Mário Granaci <202100275@estudantes.ips.pt>
2024-04-11 22:31:12 +0100

    Estatisticas

--- LusoHealth/LusoHealthClient/LusoHealthClient.Server/Models/Professionals/Service.cs
+++ LusoHealth/LusoHealthClient/LusoHealthClient.Server/Models/Professionals/Service.cs
@@ -17,36 +17,12 @@ namespace LusoHealthClient.Server.Models.Professionals
         public bool Online { get; set; }
         public bool Presential { get; set; }
         public bool Home { get; set; }
-        public virtual ICollection<Review> Reviews { get; set; }
 
         #region Navigation Properties
+        public virtual ICollection<Review> Reviews { get; set; }
         public Professional Professional { get; set; }
         public Specialty Specialty { get; set; }
         #endregion
     }
 
 }
-
-    //////public class Service
-    //////{
-    //////    [Key]
-    //////    public int Id { get; set; }
-
-    //////    // Remova a chave estrangeira que referenciava Professional
-    //////    // [Key, ForeignKey("Professional")]
-    //////    // public string IdProfessional { get; set; }
-
-    //////    [ForeignKey("Professional")]  // Adicione esta linha para configurar a relação
-    //////    public Professional Professional { get; set; }
-
-    //////    [ForeignKey("Specialty")]
-    //////    public int IdSpecialty { get; set; }
-    //////    public double PricePerHour { get; set; }
-    //////    public bool Online { get; set; }
-    //////    public bool Presential { get; set; }
-    //////    public bool Home { get; set; }
-
-    //////    #region Navigation Properties
-    //////    public Specialty Specialty { get; set; }
-    //////    #endregion
-    //////}

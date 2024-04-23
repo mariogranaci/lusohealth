00555af339f4a913d2798764624d8025361e633b
jaime-vieira <101071059+jaime-vieira@users.noreply.github.com>
2024-04-23 17:07:19 +0100

    updates

--- LusoHealth/LusoHealthClient/LusoHealthClient.Server/Migrations/20240423134938_LusoHealth.Designer.cs
+++ LusoHealth/LusoHealthClient/LusoHealthClient.Server/Migrations/20240423134938_LusoHealth.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LusoHealthClient.Server.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240418170336_LusoHealth")]
+    [Migration("20240423134938_LusoHealth")]
     partial class LusoHealth
     {
         /// <inheritdoc />

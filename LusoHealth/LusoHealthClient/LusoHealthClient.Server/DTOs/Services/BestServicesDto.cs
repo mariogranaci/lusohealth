7957352e92967d6e4e318dbd37816846aa685c23
Mário Granaci <202100275@estudantes.ips.pt>
2024-04-25 14:52:21 +0100

    a

--- LusoHealth/LusoHealthClient/LusoHealthClient.Server/DTOs/Services/BestServicesDto.cs
+++ LusoHealth/LusoHealthClient/LusoHealthClient.Server/DTOs/Services/BestServicesDto.cs
@@ -4,12 +4,12 @@ namespace LusoHealthClient.Server.DTOs.Services
 {
     public class BestServicesDto
     {
-        public int? ServiceId { get; set; }
-        public int? SpecialtyId { get; set; }
-        public int? ProfessionalTypeId { get; set; }
-        public string? Specialty { get; set; }
-        public double? PricePerHour { get; set; }
-        public ProfessionalDto? Professional { get; set; }
-        public double? Rating { get; set; }
+        public int ServiceId { get; set; }
+        public int SpecialtyId { get; set; }
+        public int ProfessionalTypeId { get; set; }
+        public string Specialty { get; set; }
+        public double PricePerHour { get; set; }
+        public ProfessionalDto Professional { get; set; }
+        public double Rating { get; set; }
     }
 }
